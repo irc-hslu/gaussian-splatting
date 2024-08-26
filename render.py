e26cc48178abce7ab5383333c2122917e9a82a4a
Thanos Charisoudis <athanasios.charisoudis@hslu.ch>
2024-08-26 16:05:24 +0200

    add scripts to merge gaussians using vanilla 3DGS based camera models

--- render.py
+++ render.py
@@ -14,12 +14,12 @@ from scene import Scene
 import os
 from tqdm import tqdm
 from os import makedirs
-from gaussian_renderer import render
 import torchvision
-from utils.general_utils import safe_state
 from argparse import ArgumentParser
-from arguments import ModelParams, PipelineParams, get_combined_args
-from gaussian_renderer import GaussianModel
+from ants.gaussiansplatting.gaussian_renderer import render
+from ants.gaussiansplatting.utils.general_utils import safe_state
+from ants.gaussiansplatting.arguments import ModelParams, PipelineParams, get_combined_args
+from ants.gaussiansplatting.scene.gaussian_model import GaussianModel
 
 def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
     render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
